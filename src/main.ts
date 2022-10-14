fc46bead8e6a3cac9bb64f49e84b56434cf7333a
fabiano.lemos <fabiano.lemos@betha.com.br>
2022-10-14 14:56:31 -0300

    Ajustado componentes

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,34 @@
-// import the fs module, which allows us to do filesystem operations
-// fs comes from nodejs, this is impossible with normal javascript
-// running in a browser.
-// You do not need to install this dependency, it is part of the
-// standard library.
+// Modulo que permite operação com arquivos
 const fs = require("fs");
 
-// Function to read and parse a JSON
+// Le o arquivo json
 function readJSON(filename: string) {
-  // read in the file
   const rawdata = fs.readFileSync(filename);
-  // parse the JSON into a mapping
   const benchmarkJSON = JSON.parse(rawdata);
-  // return it
   return benchmarkJSON;
 }
 
 
-// Create a markdown message from the two JSON.
+// Cria uma mensagem em formato markdown
 function createMessage(benchmark, comparisonBenchmark) : string {
-    let message = "## Result of Benchmark Tests\n";
+    let message = "## Resultado dos testes \n";
   
-    // Table Title
+    // Titulo da tabela 
     message += "| Key | Current PR | Default Branch |\n";
   
-    // Table Column Definitions
+    // Definição das colunas da tabela
     message += "| :--- | :---: | :---: |\n";
   
     for(const key in benchmark) {
-      // First Column: The key
-      // Please note the ` instead of ". This is TypeScripts
-      // format string. Everything in ${ } will be replaced.
+    
+      // Primeira coluna
       message += `| ${key}`;
   
-      // Second column: the value with 2 digits
+      // Segunda coluna
       const value = benchmark[key];
       message += `| ${value.toFixed(2)}`;
   
-      // Third column: the comparison value.
-      // If this does not work out (e.g. because the key is not defined),
-      // just output nothing
+      // Terceica coluna
       try {
           const oldValue = comparisonBenchmark[key];
           message += `| ${oldValue.toFixed(2)}`;
@@ -53,14 +43,14 @@ function createMessage(benchmark, comparisonBenchmark) : string {
   }
 
 
-  // read in the created JSON file
+  // Le o arquivo criado
 const benchmark = readJSON("benchmark.json")
 
-// read it in again - you can also create a second file if you want
+//
 const comparisonBenchmark = readJSON("benchmark.json")
 
-// Create the message
+// Criar uma mensagem 
 const message = createMessage(benchmark, comparisonBenchmark)
 
-// Log it to the terminal
+// Loga no terminal
 console.log(message)
